c4895a46efaa69f08abcab91ad19bd1e410494d5
Losdreyfedus <hamzakar13y@gmail.com>
2025-07-28 13:32:26 +0300

    Add config system with godotenv, structured logging with zerolog, and graceful shutdown to HTTP server

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,60 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "GoFintechAPI'ye hoş geldin!")
-    })
+	godotenv.Load() // .env dosyasını yükler
 
-    fmt.Println("Sunucu 8080 portunda başlatıldı...")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println("Sunucu başlatılırken hata oluştu:", err)
-    }
+	// Zerolog ayarları
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(os.Stdout)
+	log.Info().Msg("Application started")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Info().Str("path", r.URL.Path).Msg("Incoming request")
+		fmt.Fprintln(w, "GoFintechAPI welcomes you!")
+	})
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	// Graceful shutdown setup
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-quit
+		log.Info().Msg("Shutting down server...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Error().Err(err).Msg("Server forced to shutdown")
+		}
+	}()
+
+	log.Info().Msgf("Server started on port %s", port)
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Error().Err(err).Msg("Server error")
+	}
+
+	log.Info().Msg("Server exited cleanly")
 }
